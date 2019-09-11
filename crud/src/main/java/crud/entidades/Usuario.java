5e0e2ba6c0e163cc5e869bc5db44862aadeada31
florincy <simoneflorency@gmail.com>
2019-09-11 02:04:05 -0300

    Vai menina. uhh

---
+++ crud/src/main/java/crud/entidades/Usuario.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud.entidades;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author florincy
+ */
+@Entity
+@Table(name = "usuario")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u")
+    , @NamedQuery(name = "Usuario.findByCdUsuario", query = "SELECT u FROM Usuario u WHERE u.cdUsuario = :cdUsuario")
+    , @NamedQuery(name = "Usuario.findByNome", query = "SELECT u FROM Usuario u WHERE u.nome = :nome")})
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "cd_usuario")
+    private Integer cdUsuario;
+    @Size(max = 2147483647)
+    @Column(name = "nome")
+    private String nome;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer cdUsuario) {
+        this.cdUsuario = cdUsuario;
+    }
+
+    public Integer getCdUsuario() {
+        return cdUsuario;
+    }
+
+    public void setCdUsuario(Integer cdUsuario) {
+        this.cdUsuario = cdUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (cdUsuario != null ? cdUsuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.cdUsuario == null && other.cdUsuario != null) || (this.cdUsuario != null && !this.cdUsuario.equals(other.cdUsuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "crud.entidades.Usuario[ cdUsuario=" + cdUsuario + " ]";
+    }
+    
+}
---
+++ crud/src/main/webapp/crud.jsp
@@ -0,0 +1,47 @@
+<%-- 
+    Document   : index
+    Created on : 11/09/2019, 00:01:49
+    Author     : florincy
+--%>
+
+
+<%@page import="org.hibernate.Transaction"%>
+<%@page import="crud.utilidades.HibernateUtil"%>
+<%@page import="org.hibernate.Session"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<jsp:directive.page import="crud.entidades.*"/>
+<jsp:directive.page import="java.util.*"/>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <h1>Salva</h1>
+        <form method="POST"action="Salva">
+            <label for="codigo">ID:</label>  
+            <input type="text" id="codigo" id="codigo" name="codigo">
+            <label for="nome">Nome:</label>  
+            <input type="text" id="nome" id="nome" name="nome">
+            <input type="submit" id="botao1">
+        </form>
+        <h1>Busca e Atualiza</h1>
+        <%
+        String idUsuario = "111";
+        String hql = "from Usuario u where u.id='"+idUsuario+"'";
+        Session session1 = HibernateUtil.getSession();
+        Transaction tr = session1.beginTransaction();
+        Usuario user = (Usuario) session1.createQuery(hql).uniqueResult();
+        tr.commit();
+        Integer id = user.getCdUsuario();
+        String nome = user.getNome();
+        %>
+        <form method="POST" action="Atualiza">
+            <input type="text" name="codigo" value="<%=id%>">
+            <input type="text" name="nome" value="<%=nome%>">
+            <input type="submit" id="botao2">
+        </form>
+    </body>
+</html>
